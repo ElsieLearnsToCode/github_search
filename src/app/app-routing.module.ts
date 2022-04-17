c11026438d3869b363d70e38880d205caa7fc263
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-17 08:56:57 +0300

    add changes to the navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
   {path:"about", component: AboutComponent},
+  {path:"navbar", component: NavbarComponent},
 ];
 
 @NgModule({
