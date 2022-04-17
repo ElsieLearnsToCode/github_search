392b8225c0bb83fd7d4813a54dcfec9e683bd872
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-17 21:08:30 +0300

    add css rule to style the whole page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { GithubComponent } from './components/github/github.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
   {path:"about", component: AboutComponent},
   {path:"navbar", component: NavbarComponent},
+  {path: "", component: GithubComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormComponent } from './components/form/form.component';
+import { GithubComponent } from './components/github/github.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    NavbarComponent
+    NavbarComponent,
+    FormComponent,
+    GithubComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
