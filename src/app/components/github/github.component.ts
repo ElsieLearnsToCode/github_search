788f61ea502d4045ec8dc8b45c7daac8f260d060
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-18 12:56:47 +0300

    inject the http service into the github component

--- src/app/components/github/github.component.ts
+++ src/app/components/github/github.component.ts
@@ -1,4 +1,6 @@
+import { HttpServiceService } from './../../services/http-service.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-github',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpservice: HttpServiceService) { }
 
   ngOnInit(): void {
+    
   }
 
 }
