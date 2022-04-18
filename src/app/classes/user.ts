20cec930e983a83265ef9fc3a277b2ed941c6500
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-18 12:49:00 +0300

    create a method for requesting data from the API

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,5 +1,5 @@
 export class User {
-    constructor (public name:string, public public_repos:string, public avatar_url:string, public created_at:Date, public updated_at:Date, public html_url: string) {
+    constructor (public name:string, public public_repos:string, public avatar_url:string, public created_at:Date, public updated_at:Date) {
 
     }
 }
--- src/app/services/http-service.service.ts
+++ src/app/services/http-service.service.ts
@@ -4,11 +4,18 @@ import { observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../classes/user';
 import { Repository } from '../classes/repository';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpServiceService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) { 
+    
+  }
+
+  getUsers() {
+    return this.http.get(`https://api.github.com/users/api_key=%${environment.APIKey}`);
+  }
 }
