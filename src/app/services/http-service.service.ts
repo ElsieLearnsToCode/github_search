4e9ca80a561531dd4de9cec5f05d4e25286c1913
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-18 20:28:24 +0300

    add the logic for error handling

--- src/app/services/http-service.service.ts
+++ src/app/services/http-service.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { observable } from 'rxjs';
+import { count, observable, retry } from 'rxjs';
 import { User } from '../classes/user';
 import { Repository } from '../classes/repository';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 
 
 @Injectable({
@@ -19,6 +23,25 @@ export class HttpServiceService {
 // logic to get gh profile
   public getProfile (githubUserQuery: string) {
     let dataUrl = `https://api.github.com/users/${githubUserQuery}?client_id=${environment.CLIENT_ID}&client_secret=${environment.CLIENT_SECRETS}`;
+    return this.http.get("dataURL").pipe(
+      retry (),
+      catchError (this.handleErrors)
+    );
   }
+
+  public handleErrors(error:HttpErrorResponse) {
+    let errorMessage:string;
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `MESSAGE : ${error.error.message}`;
+    }
+
+    else {
+      errorMessage = `STATUS : ${error.status} MESSAGE : ${error.message}`;
+    }
+
+    return throwError(errorMessage);
+  }
+
+   
 
 }
